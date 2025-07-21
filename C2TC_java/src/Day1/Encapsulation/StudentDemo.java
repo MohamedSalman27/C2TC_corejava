0cbe3c58cb331972f0455e1f1839e00088f55a28
Dell <Dell@DESKTOP-LL6N1D9>
2025-07-21 19:55:13 +0530

    CharDemo

--- C2TC_java/src/Day1/Encapsulation/StudentDemo.java
+++ C2TC_java/src/Day1/Encapsulation/StudentDemo.java
@@ -4,6 +4,10 @@ public class StudentDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Student obj = new Student();
+		obj.id = 101;
+		obj.name =
+		System.out.println(obj.id);
 
 	}
 
