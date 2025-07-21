f30b97f3bc9500a1ad675442b63e6b25add1e62e
Dell <Dell@DESKTOP-LL6N1D9>
2025-07-21 16:27:01 +0530

    Name

--- C2TC_java/src/Day1/Encapsulation/Student.java
+++ C2TC_java/src/Day1/Encapsulation/Student.java
@@ -1,5 +1,13 @@
 package Day1.Encapsulation;
 
 public class Student {
-
+     private int id;
+     private String name;
+     public int setId(int i)
+     {
+    	 return id;
+     }
+     public void setId(int i) {
+    	 id = i ;
+     }
 }
