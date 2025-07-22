b063b6540db863d87b06fd325cb143a186a31a4b
Dell <Dell@DESKTOP-LL6N1D9>
2025-07-22 20:43:07 +0530

    OOPS

---
+++ C2TC_java/src/Daytwo/Customer.java
@@ -0,0 +1,29 @@
+package Daytwo;
+
+public class Customer {
+	private int cid;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	private String cname;
+	private String city;
+	
+	}
+	
+
+
---
+++ C2TC_java/src/Daytwo/customerDemo.java
@@ -0,0 +1,14 @@
+package Daytwo;
+
+public class customerDemo {
+	public static void main(String[] args)
+	{
+		Customer c1 = new Customer();
+		c1.setCid(101);
+		c1.setCname("raja");
+		c1.setCity("chennai");
+		System.out.println(c1);
+	}
+	
+
+}
